2e7adc99c61b67b504ba86c38cac85405f8e8748
puneesh singh <puneesh.singh@gmail.com>
2021-07-17 20:49:39 -0700

    added Plane test

--- src/LiveProjectTest.java
+++ src/LiveProjectTest.java
@@ -3,5 +3,7 @@ public class LiveProjectTest {
         System.out.println("Live project");
 
         Plane plane = new Jet("Fighter");
+        Jet jet = (Jet)plane;
+        System.out.println(jet.getType());
     }
 }
